e8097dbb4b29c45515254704ccca51f29d78125c
jeffx-3 <geoffreygroovy@gmail.com>
2024-11-11 12:07:09 +0300

    created views for the project

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+'''You can as well modify the views her to ensure it corresponds to the changes ou might have made in the views.py file'''
 urlpatterns = [
     #change this view with the main index rather page view
-    path('', views.test),
+    path('', views.home),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('stories/', views.stories),
+    path('products/', views.products),
+    
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+
+'''You can actualy rename the views according to the templates you'll create '''
+
 # Create your views here.
-def test(request):
-    return render(request, 'test.html')
+def home(request):
+    return render(request, 'home.html')
+
+#about view
+def about(request):
+    return render(request, 'about.html')
+
+#contact view
+def contact(request):
+    return render(request, 'contact.html')
+
+#stories view
+def stories(request):
+    return render(request, 'stories.html')
+
+#products view
+def products(request):
+    return render(request, 'products.html')
+
+
+    
